ddb44288e45d2982a8c16d0e1e02a132976f0944
Harshal Jariwala <jariwalaharshal64@gmail.com>
2024-07-30 19:46:01 +0530

    Module - 2

--- Module 2/module_2/bin/question_46.dart
+++ Module 2/module_2/bin/question_46.dart
@@ -1,33 +1,103 @@
+import 'dart:io';
+
+List<String> colors = [];
+
+String colourInputNotContains() {
+  stdout.write("Enter Color Name : ");
+  String clrName = (stdin.readLineSync()!).toUpperCase();
+  if(!colors.contains(clrName)) {
+    return clrName;
+  } else {
+    return "0";
+  }
+}
+
+String colourInputContains() {
+  stdout.write("Enter Color Name : ");
+  String clrName = (stdin.readLineSync()!).toUpperCase();
+  if(colors.contains(clrName)) {
+    return clrName;
+  } else {
+    return "0";
+  }
+}
+
+void addColor() {
+  String clr = colourInputNotContains();
+  if(clr != "0") {
+    colors.add(clr);
+    showColors();
+  } else {
+    print("Color Already In List");
+  }
+}
+
+void removeColor() {
+  String clr = colourInputContains();
+  if(clr != "0") {
+    colors.remove(clr);
+    showColors();
+  } else {
+    print("Color Not In List");
+  }
+}
+
+void updateColor() {
+  String clr = colourInputContains();
+  if(clr != "0") {
+    String newClr = colourInputNotContains();
+    if(newClr != "0") {
+      colors[colors.indexOf(clr)] = newClr;
+      showColors();
+    } else {
+      print("Color Already In List");
+    }
+  } else {
+    print("Color Not In List");
+  }
+}
+
+void showColors() {
+  var i = 1;
+  if(colors.isNotEmpty) {
+    for (String color in colors) {
+      print("${i++} : $color");
+    }
+  } else {
+    print("No Colors In List");
+  }
+}
+
 void main() {
-  List<String> fruits = <String>["Apple","Banana","Grapes"];
+  int ch;
+  do {
+    print("\n1. Add Color");
+    print("2. Remove Color");
+    print("3. Update Color");
+    print("4. Show All Colors");
+    print("5. Exit");
+    stdout.write("Enter Your Choice : ");
+    ch = int.parse(stdin.readLineSync()!);
 
-  //print list using for loop
-  for(int i = 0;i < fruits.length; i++) {
-    print("Index $i : ${fruits[i]}");
-  }
+    switch(ch) {
+      case 1:
+        addColor();
+        break;
+      case 2:
+        removeColor();
+        break;
+      case 3:
+        updateColor();
+        break;
+      case 4:
+        showColors();
+        break;
+      case 5:
+        print("Exit....");
+        break;
+      default:
+        print("Invalid Choice!!!");
+    }
 
-  //print list using for-in loop
-  for(String fruit in fruits) {
-    print(fruit);
-  }
-
-  //get index of element
-  print("Index Of Apple : ${fruits.indexOf('Apple')}");
-
-  //add element
-  fruits.add("Watermelon");
-  print(fruits);
-
-  //add element on specify index
-  fruits.insert(1, "Mango");
-  print(fruits);
-
-  //remove element
-  fruits.remove("Banana");
-  print(fruits);
-
-  //remove on specify index
-  fruits.removeAt(2);
-  print(fruits);
-
+  } while(ch != 5);
 }
