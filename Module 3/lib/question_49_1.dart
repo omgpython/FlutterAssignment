1ef8e1939a2d7327b092ee04a1220f4b22426fb5
Harshal <jariwalaharshal64@gmail.com>
2024-08-23 16:22:26 +0530

    Module-3

---
+++ Module 3/lib/question_49_1.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Question 49-1',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
